e6991d3742d32b26678934b809f2f7d0c9fd9803
Rico <ricokan@users.noreply.github.com>
2023-11-28 19:33:57 -0500

    commit

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
-// simple change model
-document.getElementById("change-colour").addEventListener("click", function() {
-    document.getElementById("model").src = "/models/teal.glb";
-});
-
-// example with showing content
-document.getElementById("change-colour").addEventListener("click", function() {
-    document.getElementById("model-info").style.display = "block";
-});
-
-// example with multiple models
-var myModels = ["./models/orange.glb", "./models/teal.glb", "./models/orange.glb", "./models/teal.glb",]
-var counter = 0;
-document.getElementById("change-colour").addEventListener("click", function() {
-    counter++;
-    if (counter==3) { 
-        counter = 0;
+//example with multiple models
+var mybed = ["./models/bed.glb", "./models/bed2.glb"]
+var counterBed = 0;
+document.getElementById("change-bed").addEventListener("click", function() {
+    counterBed++;
+    if (counterBed==2) { 
+        counterBed = 0;
     }
-    document.getElementById("model").src = myModels[counter];
+    document.getElementById("bed").src = mybed[counterBed];
 });
 
+var mybeanbag = ["./models/bean_bag.glb", "./models/bean_bag2.glb", "./models/bean_bag3.glb"]
+var counterBeanBag = 0;
+document.getElementById("change-beanbag").addEventListener("click", function() {
+    counterBeanBag++;
+    if (counterBeanBag==3) { 
+        counterBeanBag = 0;
+    }
+    document.getElementById("beanbag").src = mybeanbag[counterBeanBag];
+});
+
+var mybeanbagsize = ["./models/bean_bag.glb", "./models/bean_bagL.glb"]
+var counterBeanBagsize = 0;
+document.getElementById("change-size").addEventListener("click", function() {
+    counterBeanBagsize++;
+    if (counterBeanBagsize==2) { 
+        counterBeanBagsize = 0;
+    }
+    document.getElementById("beanbag").src = mybeanbagsize[counterBeanBagsize];
+});
